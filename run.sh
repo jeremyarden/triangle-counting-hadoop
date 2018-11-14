fdbb37fe5bf984f886dcab7f5ea63fd3f21ca5e0
Wenny Yustalim <wennyyustalim@gmail.com>
2018-11-14 16:00:11 +0700

    Modify run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 hdfs dfs -rm -r -f /user/wennyyustalim/output
 
 rm Triangle*.class
-hadoop com.sun.tools.javac.Main TriangleCount.java
-jar cf triangle.jar Triangle*.class
 
-hadoop jar triangle.jar TriangleCount /user/wennyyustalim/input /user/wennyyustalim/output
+hadoop com.sun.tools.javac.Main ClosedTripletCount.java
+jar cf triplet.jar ClosedTriplet*.class
+
+hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
