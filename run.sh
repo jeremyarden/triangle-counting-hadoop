c24e74896cba2ac94738d2fa015001c440b7cca0
Wenny Yustalim <wennyyustalim@gmail.com>
2018-11-18 18:18:19 +0700

    Modify run

--- run.sh
+++ run.sh
@@ -12,4 +12,10 @@ jar cf triplet.jar ClosedTripletCount*.class
 
 #hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
 #hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input2 /user/wennyyustalim/output2
-hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input3 /user/wennyyustalim/output3
+#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input3 /user/wennyyustalim/output3
+
+hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc1 /user/wennyyustalim/output_tc1
+#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc2 /user/wennyyustalim/output_tc2
+#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc3 /user/wennyyustalim/output_tc3
+
+hdfs dfs -cat /user/wennyyustalim/output_tc1/*
