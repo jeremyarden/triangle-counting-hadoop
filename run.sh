3756d2321d31ae169c1602d1467d04d63c486f4e
wennyyustalim <13515002@std.stei.itb.ac.id>
2018-11-18 04:00:37 +0000

    Modify run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,6 @@ rm ClosedTriplet*.class
 hadoop com.sun.tools.javac.Main ClosedTripletCount.java
 jar cf triplet.jar ClosedTripletCount*.class
 
-hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
+#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
+#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input2 /user/wennyyustalim/output2
+hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input3 /user/wennyyustalim/output3
