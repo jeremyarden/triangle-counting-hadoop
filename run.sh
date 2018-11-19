c84406f23912bd7f26b8f7aa2e8184c5fe7cf6e0
wennyyustalim <13515002@std.stei.itb.ac.id>
2018-11-19 01:55:54 +0000

    Modify run.sh

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ rm ClosedTriplet*.class
 hadoop com.sun.tools.javac.Main ClosedTripletCount.java
 jar cf triplet.jar ClosedTripletCount*.class
 
-#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
+hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
 #hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input2 /user/wennyyustalim/output2
 #hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input3 /user/wennyyustalim/output3
 
-hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc1 /user/wennyyustalim/output_tc1
+#hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc1 /user/wennyyustalim/output_tc1
 #hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc2 /user/wennyyustalim/output_tc2
 #hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc3 /user/wennyyustalim/output_tc3
 
-hdfs dfs -cat /user/wennyyustalim/output_tc1/*
+hdfs dfs -cat /user/wennyyustalim/output/*
