61752fe3410d8953c1046ceceb143b876d43ae60
wennyyustalim <13515002@std.stei.itb.ac.id>
2018-11-19 07:24:07 +0000

    Modify run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,12 @@
 
 #### Delete intermediate and output files ####
 
-hdfs dfs -rm -r -f /user/wennyyustalim/output
+#hdfs dfs -rm -r -f /user/wennyyustalim/output
+#hdfs dfs -rm -r -f /user/wennyyustalim/output1
+#hdfs dfs -rm -r -f /user/wennyyustalim/output2
+hdfs dfs -rm -r -f /user/wennyyustalim/output_tc1
+#hdfs dfs -rm -r -f /user/wennyyustalim/output_tc2
+#hdfs dfs -rm -r -f /user/wennyyustalim/output_tc3
 hdfs dfs -rm -r -f /user/wennyyustalim/temp/first-mapreduce
 hdfs dfs -rm -r -f /user/wennyyustalim/temp/second-mapreduce
 rm triplet.jar
@@ -10,11 +15,11 @@ rm ClosedTriplet*.class
 rm TriangleTypePartition*.class LongPair*.class
 
 #### Compile ####
-# hadoop com.sun.tools.javac.Main ClosedTripletCount.java
-# jar cf triplet.jar ClosedTripletCount*.class
+hadoop com.sun.tools.javac.Main ClosedTripletCount.java
+jar cf triplet.jar ClosedTripletCount*.class
 
-hadoop com.sun.tools.javac.Main TriangleTypePartition.java LongPair.java
-jar cf ttp.jar TriangleTypePartition*.class LongPair*.class
+# hadoop com.sun.tools.javac.Main TriangleTypePartition.java LongPair.java
+# jar cf ttp.jar TriangleTypePartition*.class LongPair*.class
 
 #### Execute ####
 
@@ -28,8 +33,8 @@ jar cf ttp.jar TriangleTypePartition*.class LongPair*.class
 # hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input3 /user/wennyyustalim/output3
 # hdfs dfs -cat /user/wennyyustalim/output3/*
 
-# hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc1 /user/wennyyustalim/output_tc1
-# hdfs dfs -cat /user/wennyyustalim/output_tc1/*
+hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc1 /user/wennyyustalim/output_tc1
+hdfs dfs -cat /user/wennyyustalim/output_tc1/*
 
 # hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc2 /user/wennyyustalim/output_tc2
 # hdfs dfs -cat /user/wennyyustalim/output_tc2/*
@@ -38,5 +43,5 @@ jar cf ttp.jar TriangleTypePartition*.class LongPair*.class
 # hdfs dfs -cat /user/wennyyustalim/output_tc3/*
 
 ## TTP ##
-hadoop jar ttp.jar TriangleTypePartition /user/wennyyustalim/input2 /user/wennyyustalim/output2
-hdfs dfs -cat /user/wennyyustalim/output2/*
+# hadoop jar ttp.jar TriangleTypePartition /user/wennyyustalim/input2 /user/wennyyustalim/output2
+# hdfs dfs -cat /user/wennyyustalim/output2/*
