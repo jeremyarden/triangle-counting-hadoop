4ce6d7a953b75895aafc634159a7012e6cea4e1c
Wenny Yustalim <wennyyustalim@gmail.com>
2018-11-19 21:27:45 +0700

    Delete unused files

--- run.sh
+++ run.sh
@@ -2,30 +2,24 @@
 
 #### Delete intermediate and output files ####
 
-# hdfs dfs -rm -r -f /user/wennyyustalim/output
+hdfs dfs -rm -r -f /user/wennyyustalim/output
 # hdfs dfs -rm -r -f /user/wennyyustalim/output1
 # hdfs dfs -rm -r -f /user/wennyyustalim/output2
 # hdfs dfs -rm -r -f /user/wennyyustalim/output_tc1
-hdfs dfs -rm -r -f /user/wennyyustalim/output_tc2
+# hdfs dfs -rm -r -f /user/wennyyustalim/output_tc2
 # hdfs dfs -rm -r -f /user/wennyyustalim/output_tc3
 hdfs dfs -rm -r -f /user/wennyyustalim/temp/first-mapreduce
 hdfs dfs -rm -r -f /user/wennyyustalim/temp/second-mapreduce
 rm triplet.jar
 rm ClosedTriplet*.class
-rm TriangleTypePartition*.class LongPair*.class
 
 #### Compile ####
 hadoop com.sun.tools.javac.Main ClosedTripletCount.java
 jar cf triplet.jar ClosedTripletCount*.class
 
-# hadoop com.sun.tools.javac.Main TriangleTypePartition.java LongPair.java
-# jar cf ttp.jar TriangleTypePartition*.class LongPair*.class
-
 #### Execute ####
-
-## Basic Algo ##
-# hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
-# hdfs dfs -cat /user/wennyyustalim/output/*
+hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input /user/wennyyustalim/output
+hdfs dfs -cat /user/wennyyustalim/output/*
 
 # hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input2 /user/wennyyustalim/output2
 # hdfs dfs -cat /user/wennyyustalim/output2/*
@@ -36,12 +30,8 @@ jar cf triplet.jar ClosedTripletCount*.class
 # hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc1 /user/wennyyustalim/output_tc1
 # hdfs dfs -cat /user/wennyyustalim/output_tc1/*
 
-hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc2 /user/wennyyustalim/output_tc2
-hdfs dfs -cat /user/wennyyustalim/output_tc2/*
+# hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc2 /user/wennyyustalim/output_tc2
+# hdfs dfs -cat /user/wennyyustalim/output_tc2/*
 
 # hadoop jar triplet.jar ClosedTripletCount /user/wennyyustalim/input_tc3 /user/wennyyustalim/output_tc3
 # hdfs dfs -cat /user/wennyyustalim/output_tc3/*
-
-## TTP ##
-# hadoop jar ttp.jar TriangleTypePartition /user/wennyyustalim/input2 /user/wennyyustalim/output2
-# hdfs dfs -cat /user/wennyyustalim/output2/*
